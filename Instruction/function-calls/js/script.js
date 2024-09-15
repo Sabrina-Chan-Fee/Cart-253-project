065864396bffac2f65fa342197325c4acdc0688a
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-15 15:52:20 -0400

    complete first function call example

--- Instruction/function-calls/js/script.js
+++ Instruction/function-calls/js/script.js
@@ -1,24 +1,20 @@
 /**
- * Title of Project
- * Author Name
+ * Function call
+ * Sabrina Chan Fee
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on the functioncalls example
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+//set up happens ONCE at the beginning o fthe program
 function setup() {
-
+    createCanvas(640,480);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+//this happend every frame
 function draw() {
-
+    background( "red");
+    rect(200, 80,240,320);  
 }
