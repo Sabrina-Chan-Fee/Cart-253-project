8179468698b90629a4e67a78a2ac5d45452f24a9
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-15 17:14:41 -0400

    create variable module

---
+++ Variables/create variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * create variable
+ * Sabrina Chan Fee 
+ * 
+ * intro to varibale , creat a variable
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
