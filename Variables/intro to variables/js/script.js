ba7ff623be953511e94039b7e4a2fc56112ea70a
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-15 17:12:53 -0400

    complete variable example

--- Variables/intro to variables/js/script.js
+++ Variables/intro to variables/js/script.js
@@ -11,7 +11,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(100, 480);
 }
 
 
@@ -19,5 +19,14 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    //draw circle
+    push();
+    fill(255, 255, 0);
+    noStroke();
+    // ellipse(width / 2, height / 2, 100, 100);
+    ellipse(mouseX, mouseY, 100, 100);
+
+    pop();
 }
