5955a875da4021b3d337b9c1554d09349da6d98c
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-15 16:12:51 -0400

    added ellispe

--- Instruction/function-calls/js/script.js
+++ Instruction/function-calls/js/script.js
@@ -1,20 +1,46 @@
 /**
- * Function call
+ * The plank page
  * Sabrina Chan Fee
  * 
- * Working on the functioncalls example
+ * an explorotion of the existential angst of a novelist
+ * 
+ * the program is non-interactive to convey inability to start a project
  */
 
 "use strict";
-
+/**
+ * Create the canva for our master piece
+ */
 //set up happens ONCE at the beginning o fthe program
 function setup() {
-    createCanvas(640,480);
+    //create canvas at a stardar resoltuion
+    createCanvas(640,640);
 }
 
-
+/**
+ * draws the writers piece of paper
+ */
 //this happend every frame
 function draw() {
+    //pink destop
     background( "red");
-    rect(200, 80,240,320);  
+    //the blank piece of paper
+
+    push();
+    fill("blue") ;
+    stroke(255);
+    ellipse(320,320,480);
+    pop();
+
+    push();
+    fill("white");
+    noStroke();
+    ellipse( 320,320,140,140);
+    pop();
+
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320,320,20);
+    pop();
 }
