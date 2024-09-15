284c8f1848ef83e5939e3c1fe03e307c54cd2c0f
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-15 17:31:08 -0400

    complete encapsulation

--- Variables/self-esteem/js/script.js
+++ Variables/self-esteem/js/script.js
@@ -14,14 +14,26 @@ let skyBlue = 250;
 
 // The sun
 
-// Colour
-let sunRed = 255;
-let sunGreen = 255;
-let sunBlue = 0;
-// Position and size
-let sunX = 500;
-let sunY = 70;
-let sunSize = 100;
+// // Colour
+// let sunRed = 255;
+// let sunGreen = 255;
+// let sunBlue = 0;
+// // Position and size
+// let sunX = 500;
+// let sunY = 70;
+// let sunSize = 100;
+
+let sun = {
+    fill: {
+        red: 255,
+        green: 255,
+        blue: 0,
+    },
+
+    X: 500,
+    Y: 70,
+    Size: 100,
+}
 
 // My self-esteem
 
@@ -49,9 +61,9 @@ function draw() {
 
     // The sun
     push();
-    fill(sunRed, sunGreen, sunBlue);
+    fill(sun.fill.red, sun.fill.green, sun.fill.blue);
     noStroke();
-    ellipse(sunX, sunY, sunSize);
+    ellipse(sun.X, sun.Y, sun.Size);
     pop();
 
     // My self esteem
