cc70aebabd4424c98682d202fb9a72e6aee8ce89
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-28 02:03:52 -0400

    start simple javascript example

---
+++ Events/plain javascrip/js/script.js
@@ -0,0 +1,23 @@
+/**
+ *Plain javascript
+ *Sabrina Chan Fee
+ experiment with event handling with plain javascript
+ * !
+ */
+
+"use strict";
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+
+/**
+ * 
+*/
+function draw() {
+
+}
