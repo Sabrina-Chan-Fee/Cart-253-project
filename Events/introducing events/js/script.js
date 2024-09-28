9fc8c1dd41f539573f6caa5ca8a5d947a35d8df5
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-09-28 01:19:03 -0400

    made event example

--- Events/introducing events/js/script.js
+++ Events/introducing events/js/script.js
@@ -1,24 +1,37 @@
 /**
- * Title of Project
- * Author Name
+ * Intro to events
+ * Sabrina Chan Fe
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Taking a look at how events work in javascrip and p5
+ * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ *Creates the canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Does nothing
 */
 function draw() {
 
+}
+
+/**
+ * The mousePressed() function is AUTOMATICALLY CALLED BY p5
+ * whenever the mouse button is pressed down! Handy!
+ */
+function mousePressed() {
+    // Draw an ellipse on the canvas when the mouse is pressed down
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
