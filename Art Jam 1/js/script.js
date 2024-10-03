4695d79acddcf2342bc439ffb6a0a93fbbc52523
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-10-03 00:06:49 -0400

    create main game compoment
    set up canvas, flour, chocolate, strawberry, mixing bowl and cake batter attributes

--- Art Jam 1/js/script.js
+++ Art Jam 1/js/script.js
@@ -15,3 +15,115 @@
 
 "use strict";
 
+//canvas 
+const canvas = {
+    //size
+    weight: 600,
+    height: 600,
+};
+//Mixing Bowl
+const mixingBowl = {
+    // position
+    x: canvas.weight / 3,
+    y: canvas.height / 3,
+
+    //size
+    size: 300,
+
+    // color
+    color: "#ff0000",
+};
+
+//cake batter
+const cakeBatter = {
+    // position
+    x: canvas.weight / 3,
+    y: canvas.height / 3,
+
+    //size
+    size: 260,
+
+    // color
+    color: {
+        r: 180,
+        g: 150,
+        b: 120,
+    },
+
+};
+
+//Ingredient: Strawberry
+const Strawberry = {
+    //position
+    x: 100,
+    y: 500,
+
+    //size
+    size: 100,
+
+    // color
+    color: {
+        r: 255,
+        g: 192,
+        b: 203,
+    },
+
+    //to make strawberry appear/disappear
+    visible: true,
+    //to check if object can be dragged
+    dragging: false,
+};
+
+//Ingredient: Chocolate
+const Chocolate = {
+    //position
+    x: 300,
+    y: 500,
+
+    //size
+    size: 100,
+
+    // color
+    color: {
+        r: 101,
+        g: 67,
+        b: 33,
+    },
+
+    //to make strawberry appear/disappear
+    visible: true,
+    //to check if object can be dragged
+    dragging: false,
+};
+
+//Ingredient: Flour
+const Flour = {
+    //position
+    x: 500,
+    y: 500,
+
+    //size
+    size: 100,
+
+    // color
+    color: {
+        r: 250,
+        g: 235,
+        b: 215,
+    },
+
+    //to make strawberry appear/disappear
+    visible: true,
+    //to check if object can be dragged
+    dragging: false,
+
+};
+
+
+
+
+//create the canvas
+function setup() {
+    createCanvas(canvas.weight, canvas.height);
+}
+
