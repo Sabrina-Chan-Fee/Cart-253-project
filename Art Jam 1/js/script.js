1187bbf5dd59bd2aa9e64a7676d24b60c97c9127
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-10-03 00:04:51 -0400

    Create art jam project

---
+++ Art Jam 1/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Mixing Cake batter
+ * By Sabrina Chan Fee
+ * 
+ * A game where the player addes the ingredients and mixes the cake batter in the bowl!
+ * 
+ * Controls: 
+ * - drag and drop ingredients into the bowl
+ * - circulate mouse in bowl to mix everything togther
+ * 
+ * Uses:
+ * p5.js
+ * https://p5js.org
+ */
+
+"use strict";
+
