8d5c5cd361609d67d7a12976b2488484adc36281
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-10-24 13:01:41 -0400

    start speach speach project

---
+++ array/speachspeach/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * speech speech
+ * Sabrina Chan Fee
+ * 
+ * Interactive speech playing interface
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
