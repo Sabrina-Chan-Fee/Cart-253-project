91e1a910f5ca6d005705c2c809b612d3bee9a18e
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-10-24 13:11:45 -0400

    finish speak speak

--- array/speachspeach/js/script.js
+++ array/speachspeach/js/script.js
@@ -7,11 +7,16 @@
 
 "use strict";
 
+//speakc itself
+const speech = ["Veni", "Vidi", "Vici", "Sensi malum."];
+//index
+let speechIndex = 0;
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
 
@@ -19,5 +24,21 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
+    let currentLine = speech[speechIndex];
+    //display line
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    speechIndex++;
+    if (speechIndex >= speech.length)
+        speechIndex = 0;
+
 
 }
