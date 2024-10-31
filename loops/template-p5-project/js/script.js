077d2aa802ad823a348f06bd3508b724c10445ae
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-10-31 14:54:31 -0400

    start for loop project draw a star field

---
+++ loops/template-p5-project/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * Draw a star field with for loop!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
