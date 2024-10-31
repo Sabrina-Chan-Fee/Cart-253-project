95cf1a5f494411fe5d23a3c2f6b2f39413431c91
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-10-31 15:02:18 -0400

    finish drawing star field

--- loops/template-p5-project/js/script.js
+++ loops/template-p5-project/js/script.js
@@ -7,10 +7,13 @@
 
 "use strict";
 
+const numStars = 100;
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(400, 400);
 
 }
 
@@ -19,5 +22,23 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0);
 
+    randomSeed(1);
+    for (let i = 0; i < numStars; i++) {
+        drawStar();
+    }
+
+}
+
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(0, 2);
+
+    push();
+    fill(255);
+    noStroke();
+    ellipse(x, y, diameter);
+    pop();
 }
