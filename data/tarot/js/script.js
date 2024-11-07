c3ff4228369a902cb30c3c700584ba966c56d295
Sabrina-Chan-Fee <119706202+Sabrina-Chan-Fee@users.noreply.github.com>
2024-11-07 15:06:03 -0500

    complete tarot project json

--- data/tarot/js/script.js
+++ data/tarot/js/script.js
@@ -7,17 +7,45 @@
 
 "use strict";
 
-/**
- * tbd.
-*/
-function setup() {
+//out tarot data
+let tarot = undefined;
+let fortune = "click for a fortune";
 
+/**
+ * Load tarot dat
+ */
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
 }
 
 
 /**
- * tbd.
+ * create canvas
+*/
+function setup() {
+    createCanvas(800, 400);
+}
+
+
+/**
+ * display tarot
 */
 function draw() {
+    background(0);
+
+    //display the information
+    push();
+    textSize(16);
+    fill("yellow");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    // Choose a random card
+    let card = random(tarot.tarot_interpretations);
+    // Choose a random fortune
+    fortune = random(card.fortune_telling);
 
 }
